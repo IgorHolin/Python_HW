c9d58a8860c3dc8f47b93a046f0f2df21a9211a9
Igor <igor.paluhovicz@gmail.com>
2022-09-06 13:44:24 +0200

    Ex_3_done

--- HW/HW_1.py
+++ HW/HW_1.py
@@ -1,17 +1,24 @@
-# 1 Напишите программу, которая принимает на вход цифру, обозначающую день недели
-# и проверяет, является ли этот день выходным.
+# 3 Напишите программу, которая принимает на вход координаты точки (X и Y)
+# причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости
+# в которой находится эта точка (или на какой оси она находится).
 
 # *Пример:
-# - 6 -> да
-# - 7 -> да
-# - 1 -> нет
+# - x=34; y=-30 -> 4
+# - x=2; y=4-> 1
+# - x=-34; y=-30 -> 3
 
-while True: #сделал бесконечный цикл чтоб не перезапускать программу миллион раз
-    day = int(input("Enter the number of the day please: "))
-    if 1 <= day <= 5:
-        print('You have to go to work :(')
-    elif 6 <= day <=7:
-        print("It's day off!! :)")
+
+while True: #использую бесконечный цикл, чтоб не перезапускать программу
+
+    x = float(input("Enter 'x' please: ")) 
+    y = float(input("Enter 'y' please: "))
+    #использую float значение так как не сказано что числа целые
+    
+    if x > 0 and y > 0:
+        print('1st quarter')
+    elif x < 0 and y > 0:
+        print('2nd quarter')
+    elif x < 0 and y < 0:
+        print('3rd quarter')
     else:
-        print("There are only 7 days in week -_-'")
- 
+        print('4th quarter')
--- HW/README.md
+++ HW/README.md
@@ -1,2 +1,3 @@
-Every new excersise will be located on separate branch named Ex_'number'
-For example: Ex_1 Ex_2 etc.
+Every new excersise will be located on separate branch named Ex_'number', except 1st excersise.
+1st excersise will be in "main" branch.
+For example: Ex_2 Ex_3 etc.
