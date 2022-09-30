4163516b30af90632dbf748c52c5702090fc5a1a
Igor <igor.paluhovicz@gmail.com>
2022-09-30 15:08:07 +0200

    ui added

--- HW/Seminari/Seminar_7/ui.py
+++ HW/Seminari/Seminar_7/ui.py
@@ -1,16 +1,16 @@
 from calc import x,y
 
-def which_num():
-    which_num = input('Чтобы выбрать рациональные числа введите "1", чтобы выбрать натуральные введите "2": ')
-    while which_num not in "12":
-        which_num = input('Введите "1" - рациональные числа или "2" - натуральные числа: ')
-    if which_num == "1":
+def which_type():
+    w_type = input('Чтобы выбрать рациональные числа введите "1", чтобы выбрать натуральные введите "2": ')
+    while w_type not in "12":
+        w_type = input('Введите "1" - рациональные числа или "2" - натуральные числа: ')
+    if w_type == "1":
         type(x), type(y) == int
         print('Вы работаете с натуральными числами')
-    elif which_num == "2":
+    elif w_type == "2":
         type(x), type(y) == float
         print('Вы работаете с рациональными числами')
-    return which_num
+    return w_type
         
 
 def get_action():
@@ -27,5 +27,8 @@ def get_action():
         print('Вы выбрали деление')
     return which_action
 
-def get_value():
-    return int(input('Введите число: '))
+def get_value(value_type):
+    if value_type == "1":
+        return float(input('Введите число: '))
+    else:
+        return int(input('Введите число: '))
