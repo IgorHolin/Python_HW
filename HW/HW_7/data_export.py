e96cad9da1fddd9823ba3725a31f59f1ebfc8481
Igor <igor.paluhovicz@gmail.com>
2022-10-09 19:56:31 +0200

    export

---
+++ HW/HW_7/data_export.py
@@ -0,0 +1,21 @@
+from show_data import show_data as sd
+
+def create_html():
+    output = sd().split()
+    style = 'style="font-size:18px;"'
+    html = '<html>\n  <head></head>\n  <body>\n'
+    html += '    <p {}>База:\n {} </p>\n'\
+        .format(style, output)
+
+    with open('C:\\Users\\User\\Desktop\\GeekBrains\\Знакомство с языком Python\\Семинары\\HW\\HW_7\\data_.html', 'w', encoding="UTF-8") as page:
+        page.writelines(html)
+# create_html()
+
+def create_xml():
+    xml = '<xml>\n'
+    xml += '    База:\n{}'\
+        .format(sd().split())
+
+    with open('C:\\Users\\User\\Desktop\\GeekBrains\\Знакомство с языком Python\\Семинары\\HW\\HW_7\\data_.xml', 'w', encoding="UTF-8") as page:
+        page.write(xml)
+# create_xml()
